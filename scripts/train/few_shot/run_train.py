98c49330caecf6b3f66e9d6abf7f71be38c75994
12ds95 <957517653@qq.com>
2018-05-17 23:53:08 +0800

    decay -> 20

--- scripts/train/few_shot/run_train.py
+++ scripts/train/few_shot/run_train.py
@@ -23,7 +23,7 @@ parser.add_argument('--data.test_shot', type=int, default=0, metavar='TESTSHOT',
                     help="number of support examples per class in test. 0 means same as data.shot (default: 0)")
 parser.add_argument('--data.test_query', type=int, default=15, metavar='TESTQUERY',
                     help="number of query examples per class in test. 0 means same as data.query (default: 15)")
-parser.add_argument('--data.train_episodes', type=int, default=500, metavar='NTRAIN',
+parser.add_argument('--data.train_episodes', type=int, default=100, metavar='NTRAIN',
                     help="number of train episodes per epoch (default: 100)")
 parser.add_argument('--data.test_episodes', type=int, default=1000, metavar='NTEST',
                     help="number of test episodes per epoch (default: 100)")
@@ -52,7 +52,7 @@ parser.add_argument('--train.optim_method', type=str, default='Adam', metavar='O
                     help='optimization method (default: Adam)')
 parser.add_argument('--train.learning_rate', type=float, default=0.001, metavar='LR',
                     help='learning rate (default: 0.0001)')
-parser.add_argument('--train.decay_every', type=int, default=10, metavar='LRDECAY',
+parser.add_argument('--train.decay_every', type=int, default=20, metavar='LRDECAY',
                     help='number of epochs after which to decay the learning rate')
 default_weight_decay = 0.0
 parser.add_argument('--train.weight_decay', type=float, default=default_weight_decay, metavar='WD',
